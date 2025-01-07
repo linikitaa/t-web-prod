f2535ed332a1bcefd7e436cb756c89af7d0ab845
linikitaa <linikitaa@yandex.ru>
2025-01-07 17:02:32 +0300

    add components input, text

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,33 @@
+import s from './Input.module.scss'
+import clsx from 'clsx'
+import { ChangeEvent, memo } from 'react'
+
+interface Props {
+  className?: string
+  type?: string
+  placeholder?: string
+  id?: string
+  onChange?: (value: string) => void
+  value?: string
+}
+
+export const Input = memo(
+  ({ className, id, type = 'text', onChange, placeholder, value }: Props) => {
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e.currentTarget.value)
+    }
+
+    return (
+      <div className={clsx(s.InputWrapper, className)}>
+        <input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          id={id}
+          className={s.input}
+          onChange={onChangeHandler}
+        />
+      </div>
+    )
+  },
+)
---
+++ src/shared/ui/Text/Text.tsx
@@ -0,0 +1,28 @@
+import s from './Text.module.scss'
+import clsx from 'clsx'
+
+export enum TextTheme {
+  PRIMARY = 'primary',
+  ERROR = 'error',
+}
+
+interface Props {
+  className?: string
+  title?: string
+  subtitle?: string
+  theme?: TextTheme
+}
+
+export const Text = ({
+  className,
+  title,
+  subtitle,
+  theme = TextTheme.PRIMARY,
+}: Props) => {
+  return (
+    <div className={clsx(s.Text, s[theme], className)}>
+      {title && <p className={s.title}>{title}</p>}
+      {subtitle && <p className={s.subtitle}>{subtitle}</p>}
+    </div>
+  )
+}
